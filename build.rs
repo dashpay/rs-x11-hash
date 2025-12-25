1883d4a4adf80c2288aa16020210e311927397f3
Kevin Rombach <35775977+xdustinface@users.noreply.github.com>
2025-12-25 21:27:42 +0100

    chore: drop unused `bindgen` code/dependency (#9)

--- build.rs
+++ build.rs
@@ -1,29 +1,8 @@
-extern crate bindgen;
 extern crate cc;
 
 use std::env;
-use std::path::PathBuf;
 
 fn main() {
-    // bindgen::Builder::default()
-    //     .header("src/x11/aes_helper.h")
-    //     .header("src/x11/Blake.h")
-    //     .header("src/x11/Bmw.h")
-    //     .header("src/x11/CubeHash.h")
-    //     .header("src/x11/Echo.h")
-    //     .header("src/x11/Groestl.h")
-    //     .header("src/x11/Jh.h")
-    //     .header("src/x11/Keccak.h")
-    //     .header("src/x11/Luffa.h")
-    //     .header("src/x11/Shavite.h")
-    //     .header("src/x11/Simd.h")
-    //     .header("src/x11/Skein.h")
-    //     .header("src/x11/sph_types.h")
-    //     .generate()
-    //     .expect("Error bindings generation")
-    //     .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("src.rs"))
-    //     .expect("Couldn't write bindings!");
-
     let mut cc = cc::Build::new();
     cc.file("src/x11_hash.c");
     cc.include("src");
